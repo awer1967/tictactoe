d814ed132de7f9d10fff64d07e022bab1a80d835
awer1967 <awer1967@gmail.com>
2018-10-29 10:02:56 +0200

    Добавлена функция отрисовки "решеточки" игры, оттестировано pylint

--- tictactoe.py
+++ tictactoe.py
@@ -1,20 +1,38 @@
 """Tictactoe module"""
 
-def cl_screen() :
+
+def cl_screen():
     """Clear screen function"""
     # Clear the screen before squares drawing
     print('\n'*100)
 
-def replay() :
+
+def display_board(board):
+
+    """Print board on a command line"""
+    # Print the game board on command line with filled boxes
+    cl_screen()
+    print(board[7]+'|'+board[8]+'|'+board[9])
+    print('-----')
+    print(board[4]+'|'+board[5]+'|'+board[6])
+    print('-----')
+    print(board[1]+'|'+board[2]+'|'+board[3])
+
+
+def replay():
     """Replay function
     """
     # Asking for replay if you want to play again
-    choice = input(' Do You want to play again ? Press Y to continue, any other key to quit. ').upper()
-    return choice=='Y'
+    choice = input(' Do You want to play again ? Press Y to continue, '
+                   'any other key to quit. ').upper()
+    return choice == 'Y'
 
-#The main play cycle here
+# The main play cycle here
 
 while True:
-    cl_screen()
+    # Define an list for the board and fill it in with spaces
+    THE_BOARD = [' '] * 10
+    display_board(THE_BOARD)
     if not replay():
+        cl_screen()
         break
